f715f633a4c5a62a4d266145ba6323e43f0d7d15
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-24 17:35:10 +0100

    Initial Commit

---
+++ BusinessCard SwiftUI/BusinessCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCard_SwiftUIApp.swift
+//  BusinessCard SwiftUI
+//
+//  Created by Ryan Henzell-Hill on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
